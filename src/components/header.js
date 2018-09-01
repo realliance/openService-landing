91650015237f22c3b4da2416d9c3c0d6bd6e793b
Christopher Miller <admin@mctherealm.net>
2018-08-31 20:03:20 -0400

    Added design pieces

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import logo from '../../public/static/openServiceIcon-01.svg'
+
 const Header = ({ siteTitle }) => (
   <nav className={"navbar navbar-light"} style={{backgroundColor: '#e3f2fd'}}>
-    <span className={"navbar-brand mb-0 h1"}>openService</span>
+    <span className={"navbar-brand mb-0 h1"}><object data={logo} type={"image/svg+xml"} width={"30"} height={"30"} className={"d-inline-block align-top"} /> openService</span>
     <ul className={"navbar-nav"}>
       <li className={"nav-item my-auto"}>
         <Link className={"nav-link"} to="/page-2/">Request a Website</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import { flipInX } from 'react-animations';
+import Radium, {StyleRoot} from 'radium';
+
+const styles = {
+  flipInX: {
+    animation: 'x 2s',
+    animationName: Radium.keyframes(flipInX, 'flipInX')
+  }
+}
+
+import logo from '../../public/static/openServiceLogo-01.svg'
+
 const IndexPage = () => (
-  <div>
-    <h3 className={"display-4"}>Tools for helping the community.</h3>
+  <StyleRoot>
+    <div className={"row col-md-4 offset-md-4"}>
+     <object data={logo} type={"image/svg+xml"} style={styles.flipInX} />
+    </div>
+    <h3 className={"display-4 text-center"}>Tools for helping the community.</h3>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
-  </div>
+  </StyleRoot>
 )
 
 export default IndexPage
