f37f646f0845a29eb78433fb9065610e8d4522ef
Christopher Miller <admin@mctherealm.net>
2018-09-11 16:46:00 -0400

    Added more text content

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,13 +15,16 @@ import logo from '../../public/static/openServiceLogo-01.svg'
 
 const IndexPage = () => (
   <StyleRoot>
-    <div className={"container"}>
-      <div className={"row col-md-4 offset-md-4"}>
-      <object data={logo} type={"image/svg+xml"} style={styles.flipInX} />
+    <div style={{ height: '100vh '}}>
+      <div className={"container"} style={{ paddingTop: '10vh '}}>
+        <div className={"row col-md-4 offset-md-4"}>
+        <object data={logo} type={"image/svg+xml"} style={styles.flipInX} />
+        </div>
       </div>
+      <h3 className={"display-4 text-center"}>Free. Open Source.</h3>
+      <h3 className={"display-4 text-center"}>Tools for helping the community.</h3>
     </div>
-    <h3 className={"display-4 text-center"}>Tools for helping the community.</h3>
-    <div className={"row my-4"}>
+    <div className={"row py-4"} style={{ backgroundColor: '#eeeeef' }}>
       <div className={"col-sm-12 col-md-4 text-center"}>
         <h5>Manage your Events without the hassle</h5>
         <p>Every event post is built to provide as much information as possible to interested members. Automatic event ordering keeps new, trending events towards the top, giving coverage to opportunities that need more manpower.</p>
@@ -33,6 +36,28 @@ const IndexPage = () => (
       <div className={"col-sm-12 col-md-4 text-center"}>
         <h5>All Open Source</h5>
         <p>Open Source development allows for a more transparent product, with the addition of community support for any issues. As well, this allows anyone with the know-how to host a copy for themselves.</p>
+      </div>
+    </div>
+    <div className={"container"}>
+      <div className={"my-4"}>
+        <h4>User Accounts</h4>
+        <p>Members can register on their own and then must be activated by a leader of the organization. This allows for clubs that have fees to not allow users to sign up until they have paid their dues.</p>
+      </div>
+      <div className={"my-4"}>
+        <h4>Event Creation and Sign Ups</h4>
+        <p>Organization leaders can post events for activated users to sign up for. openService will automatically organize the events by the most abundent slots avaliable, and close off full events to avoid overflow. As well, officers can generate a sign in sheet for the automatically, allowing for easy sign in once at the event. After the event, the participants can be updated and given their hours, all organized through openService.</p>
+      </div>
+      <div className={"my-4"}>
+        <h4>Users in Control</h4>
+        <p>Users can access their own dashboard to see how many hours they have earned, and a breakdown of how they achieved those hours.</p>
+      </div>
+      <div className={"my-4"}>
+        <h4>Announcements</h4>
+        <p>Leaders can publish announcement for every user to see. Whenever a user logs in, any new announcements will be displayed to them.</p>
+      </div>
+      <div className={"my-4"}>
+        <h4>Make it your Own</h4>
+        <p>Change the way your website feels using the settings panel located in the admin dashboard.</p>
       </div>
     </div>
   </StyleRoot>
